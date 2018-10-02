fda6ea2603a5f340056c28fde6214795c94dfe4a
michael <mj662560@gmail.com>
2018-10-02 08:36:09 -0400

    made action button go to diffrent web page

--- webapp/webapp/ffotf/views.py
+++ webapp/webapp/ffotf/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 def index(request):
 	context = {}
+	print("start")
+	if request.GET.get('activate'):
+		print("button pressed")
 	return render(request, 'ffotf/index.html',context)
+def action(request):
+	return HttpResponse("the button is presses")
 # Create your views here.
